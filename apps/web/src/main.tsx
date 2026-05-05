a1124c4c664384ecf419e68a5e3671ac63eb1635
Cloudygetty <cloudygetty@gmail.com>
2026-05-05 19:53:37 -0400

    feat(web): React entry point

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProxmWeb from "./ProxmWeb";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ProxmWeb />
+  </React.StrictMode>
+);
